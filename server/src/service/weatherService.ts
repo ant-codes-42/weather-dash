6d34269b10fb00b184a4d087fffe881333e3f910
ant-codes-42 <schwab.it@pm.me>
2025-01-24 09:29:34 -0600

    initial commit

---
+++ server/src/service/weatherService.ts
@@ -0,0 +1,95 @@
+//import dotenv from 'dotenv';
+//dotenv.config();
+
+// TODO: Define an interface for the Coordinates object
+
+// TODO: Define a class for the Weather object
+
+// TODO: Complete the WeatherService class
+//class WeatherService {
+  // TODO: Define the baseURL, API key, and city name properties
+  // TODO: Create fetchLocationData method
+  // private async fetchLocationData(query: string) {}
+  // TODO: Create destructureLocationData method
+  // private destructureLocationData(locationData: Coordinates): Coordinates {}
+  // TODO: Create buildGeocodeQuery method
+  // private buildGeocodeQuery(): string {}
+  // TODO: Create buildWeatherQuery method
+  // private buildWeatherQuery(coordinates: Coordinates): string {}
+  // TODO: Create fetchAndDestructureLocationData method
+  // private async fetchAndDestructureLocationData() {}
+  // TODO: Create fetchWeatherData method
+  // private async fetchWeatherData(coordinates: Coordinates) {}
+  // TODO: Build parseCurrentWeather method
+  // private parseCurrentWeather(response: any) {}
+  // TODO: Complete buildForecastArray method
+  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
+  // TODO: Complete getWeatherForCity method
+  // async getWeatherForCity(city: string) {}
+//}
+
+import dayjs, { type Dayjs } from 'dayjs';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// TODO: Define an interface for the Coordinates object
+interface Coordinates {
+  name: string;
+  lat: number;
+  lon: number;
+  country: string;
+  state: string;
+}
+
+// TODO: Define a class for the Weather object
+class Weather {
+  city: string;
+  date: Dayjs | string;
+  tempF: number;
+  windSpeed: number;
+  humidity: number;
+  icon: string;
+  iconDescription: string;
+  constructor(
+    city: string,
+    date: Dayjs | string,
+    tempF: number,
+    windSpeed: number,
+    humidity: number,
+    icon: string,
+    iconDescription: string
+  ) {
+    this.city = city;
+    this.date = date;
+    this.tempF = tempF;
+    this.windSpeed = windSpeed;
+    this.humidity = humidity;
+    this.icon = icon;
+    this.iconDescription = iconDescription;
+  }
+}
+
+// TODO: Complete the WeatherService class
+class WeatherService {
+  
+  private baseURL?: string;
+
+  private apiKey?: string;
+
+  private city = '';
+
+  constructor() {
+    this.baseURL = process.env.API_BASE_URL || '';
+
+    this.apiKey = process.env.API_KEY || '';
+  }
+  // TODO: method to fetch weather based on city 
+
+  // TODO: method to fetch 5 day forecast based on lon and lat 
+
+  // TODO: method to build and return your array of weather objects
+}
+
+//export default new WeatherService();
+
+export default new WeatherService();
